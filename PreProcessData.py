e9acae0716f7befcab5d297a302470eb93f9c98c
skuzi2 <skuzi2@illinois.edu>
2018-11-19 12:13:21 -0600

    Upload New File

---
+++ PreProcessData.py
@@ -0,0 +1,68 @@
+import numpy as np
+import os
+from TextDataset import TextDataset
+
+
+def pre_process(data_dir):
+    x = []
+    y = []
+    with open(data_dir, 'r', encoding='utf-8') as f:
+        for line in f:
+            args = line.split('\",\"')
+            label = int(args[0].lstrip('\"')) - 1
+            title = args[1].lower()
+            body = args[2].rstrip('\n').rstrip('\"').lower()
+            x.append(title + ' ' + body)
+            y.append(label)
+
+    return x, y
+
+
+def write_data_to_file(output_dir, data):
+    with open(output_dir, 'w', encoding='utf-8') as f:
+        for tokens in data:
+            for token in tokens:
+                f.write("%i " % token)
+            f.write("\n")
+
+
+def write_labels_to_file(output_dir, data):
+    with open(output_dir, 'w', encoding='utf-8') as f:
+        for label in data:
+            f.write("%i " % label)
+            f.write("\n")
+
+
+def main():
+    train_directory = './amazon_review_full_csv/train.csv'
+    test_directory = './amazon_review_full_csv/test.csv'
+
+    if not os.path.isdir('preprocessed_data'):
+        os.mkdir('preprocessed_data')
+
+    id_2_char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                   'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                   'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6',
+                   '7', '8', '9', '-', ',', ';', '.', '!', '?', ':', '’', '\"',
+                   '/', '|', '_', '#', '$', '%', 'ˆ', '&', '*', '˜', '‘', '+',
+                   '=', '<', '>', '(', ')', '[', ']', '{', '}', ' ']
+
+    char_2_id = {}
+    for i in range(len(id_2_char)):
+        char_2_id[id_2_char[i]] = i
+    unknown_id = len(id_2_char)
+
+    x_train, y_train = pre_process(train_directory)
+    x_test, y_test = pre_process(test_directory)
+    x_train_token_ids = [[char_2_id.get(token, unknown_id) for token in x] for x in x_train]
+    x_test_token_ids = [[char_2_id.get(token, unknown_id) for token in x] for x in x_test]
+
+    np.save('preprocessed_data/amazon_dictionary.npy', np.asarray(id_2_char))
+    write_data_to_file('preprocessed_data/amazon_train.txt', x_train_token_ids)
+    write_labels_to_file('preprocessed_data/amazon_train_labels.txt', y_train)
+    write_data_to_file('preprocessed_data/amazon_test.txt', x_test_token_ids)
+    write_labels_to_file('preprocessed_data/amazon_test_labels.txt', y_test)
+
+
+if __name__ == '__main__':
+    main()

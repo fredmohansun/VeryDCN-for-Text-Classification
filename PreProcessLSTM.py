f0c88225b3c50b0802bf0dd3d89194d6ff80fd71
Saar Kuzi <saarkuzi@wirelessprv-10-195-73-60.near.illinois.edu>
2018-12-07 13:19:58 -0600

    modifying the LSTM precprocessing

--- PreProcessLSTM.py
+++ PreProcessLSTM.py
@@ -5,7 +5,7 @@ import itertools
 import io
 
 
-def pre_process(data_dir):
+def pre_process_amazon(data_dir):
     x = []
     y = []
     with open(data_dir, 'r', encoding='utf-8') as f:
@@ -17,19 +17,48 @@ def pre_process(data_dir):
             text = nltk.word_tokenize(title + ' ' + body)
             x.append(text)
             y.append(label)
+    return x, y
 
+
+def pre_process_dbpedia(data_dir):
+    x = []
+    y = []
+    with open(data_dir, 'r', encoding='utf-8') as f:
+        for line in f:
+            label = int(line.split(',')[0])-1
+            text = line.split(',')
+            text = ','.join(text[1: len(text)])
+            args = text.split('\",\"')
+            title = args[0].lstrip('\"').lower()
+            body = args[1].rstrip('\n').rstrip('\"').lower()
+            text = nltk.word_tokenize(title + ' ' + body)
+            x.append(text)
+            y.append(label)
     return x, y
 
 
 def main():
-    train_directory = 'ag_news_csv/train.csv'
-    test_directory = 'ag_news_csv/test.csv'
 
-    if not os.path.isdir('preprocessed_data'):
-        os.mkdir('preprocessed_data')
+    dataset = 'dbpedia'
+    base_directory = '../project/'
 
-    x_train, y_train = pre_process(train_directory)
-    x_test, y_test = pre_process(test_directory)
+    train_directories = {'dbpedia': base_directory + 'dbpedia_csv/train.csv'}
+    test_directories = {'dbpedia': base_directory + 'dbpedia_csv/test.csv'}
+
+    if not os.path.isdir(base_directory + 'preprocessed_data_lstm'):
+        os.mkdir(base_directory + 'preprocessed_data_lstm')
+
+    x_train, y_train = [], []
+    x_test, y_test = [], []
+    if dataset == 'dbpedia':
+        x_train, y_train = pre_process_dbpedia(train_directories[dataset])
+        x_test, y_test = pre_process_dbpedia(test_directories[dataset])
+    elif dataset == 'amazon':
+        x_train, y_train = pre_process_amazon(train_directories[dataset])
+        x_test, y_test = pre_process_amazon(test_directories[dataset])
+    else:
+        print("dataset " + str(dataset) + " is not supported")
+        exit(-1)
 
     # word_to_id and id_to_word. associate an id to every unique token in the training data
     all_tokens = itertools.chain.from_iterable(x_train)
@@ -52,17 +81,17 @@ def main():
     x_test_token_ids = [[word_to_id.get(token, -1) + 1 for token in x] for x in x_test]
 
     # save dictionary
-    np.save('preprocessed_lstm_data/ag_news_dictionary.npy', np.asarray(id_to_word))
+    np.save(base_directory + 'preprocessed_data_lstm/' + dataset + '_dictionary.npy', np.asarray(id_to_word))
 
     # save training data to single text file
-    with io.open('preprocessed_lstm_data/ag_news_train.txt', 'w', encoding='utf-8') as f:
+    with io.open(base_directory + 'preprocessed_data_lstm/' + dataset + '_train.txt', 'w', encoding='utf-8') as f:
         for tokens in x_train_token_ids:
             for token in tokens:
                 f.write("%i " % token)
             f.write("\n")
 
     # save test data to single text file
-    with io.open('preprocessed_lstm_data/ag_news_test.txt', 'w', encoding='utf-8') as f:
+    with io.open(base_directory + 'preprocessed_data_lstm/' + dataset + '_test.txt', 'w', encoding='utf-8') as f:
         for tokens in x_test_token_ids:
             for token in tokens:
                 f.write("%i " % token)

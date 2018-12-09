79fc5f62b8a4db6a8e2b01b26e1621c16ce2416e
Sahiti Labhishetty <sahiti@Sahitis-MacBook-Pro.local>
2018-12-09 12:35:06 -0600

    added lr schedule

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ test_x_path = dict(zip(Datasets, list(map(concatenater,['preprocessed_data/']*le
 test_y_path = dict(zip(Datasets, list(map(concatenater,['preprocessed_data/']*len(Datasets),Datasets,['_test_labels.txt']*len(Datasets)))))
 lr_dict = {'SGD': 0.001, 'ADAM':0.01}
 batch_size_dict = {9: 200, 17: 150, 29:100, 49:75}
-save_frequency = {9: 5, 17: 2, 29:2, 49:1}
+save_frequency = {9: 10, 17: 10, 29:2, 49:1}
 
 ## Parser
 parser = argparse.ArgumentParser()
@@ -56,7 +56,7 @@ lr = lr_dict.get(optim,0.001) if args.lr is None else args.lr
 embed_size = 16 if args.embed_size is None else args.embed_size
 downsample = 1 if args.downsample is None else args.downsample
 kmaxpool = 8 if args.kmaxpool is None else args.kmaxpool
-savefile = '_'.join([dataset, str(depth), str(batch_size), str(max_epoch), optimi, ['No downsample', 'MaxPooling', 'ResNet', '{0}-max'.format(kmaxpool)][downsample]])
+savefile = '_'.join([dataset, str(depth), str(batch_size), str(max_epoch), str(lr), optimi, ['No downsample', 'MaxPooling', 'ResNet', '{0}-max'.format(kmaxpool)][downsample]])
 num_classes = 5 if args.num_classes is None else args.num_classes
 print(savefile)
 
@@ -87,7 +87,7 @@ if optimi == 'SGD':
     optimizer = optim.SGD(model.parameters(), lr = lr, momentum=0.9)
 else:
     optimizer = optim.Adam(model.parameters(), lr = lr)
-
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.7)
 criterion = nn.CrossEntropyLoss()
 criterion = criterion.cuda()
 #################################chamring block breaker#############################################
@@ -100,6 +100,7 @@ val_acc = []
 total_step = len(train_loader)
 print('start training...')
 for epoch in range(max_epoch):
+    scheduler.step()
     model.train()
     running_acc = 0.0
     running_loss = 0.0
@@ -193,8 +194,9 @@ for param in model.parameters():    #changed for pytorch 0.3.0
     param.requires_grad = True
 
 print(basic_message.format('Test', '\"validation\"', running_acc/counter*100.0, running_loss/(counter/batch_size),float(time.time()-timer1)))
-torch.save(model.state_dict(), 'model/'+savefile+'_final.model')
 np.save('state/' + savefile + 'training_acc.npy', np.array(train_acc))
 np.save('state/' + savefile + 'training_loss.npy', np.array(train_loss))
 np.save('state/' + savefile + 'val_acc.npy', np.array(val_acc))
 np.save('state/' + savefile + 'val_loss.npy', np.array(val_loss))
+torch.save(model.state_dict(), 'model/'+savefile+'_final.model')
+
